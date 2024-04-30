407d4b41308fd4f6dde188798b35b9e5bf4bb3db
emmavasylyk <emma.ovsiannikova91@gmail.com>
2024-04-30 16:11:29 +0300

    Folders setup

---
+++ app/(auth)/(routes)/sign-in/page.tsx
@@ -0,0 +1,5 @@
+const SignInPage = () => {
+  return <div className="">This is an unprotected page</div>;
+};
+
+export default SignInPage;
---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <p className="">This is a protected page</p>;
+}
