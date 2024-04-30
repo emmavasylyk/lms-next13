2cc3c48f4877aa311d4ee117fd0cf134d65a5fb7
emmavasylyk <emma.ovsiannikova91@gmail.com>
2024-04-30 15:32:20 +0300

    init starter

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="">
+      <p className="text-3xl font-medium text-sky-700">hello world</p>
+      <Button variant="destructive">Click me</Button>
+    </div>
+  );
+}
