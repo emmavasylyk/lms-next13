9a6b6ab5e5b6ee2b656bc0d2ad1fca245a1429f6
emmavasylyk <emma.ovsiannikova91@gmail.com>
2024-05-02 13:44:57 +0300

    Course Creation API

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -0,0 +1,5 @@
+const CourseIdPage = ({ params }: { params: { courseId: string } }) => {
+  return <div className="">Course Id: {params.courseId}</div>;
+};
+
+export default CourseIdPage;
--- app/test/page.tsx
+++
@@ -1,5 +0,0 @@
-const TestPage = () => {
-  return <div className="">This should be unprotected</div>;
-};
-
-export default TestPage;
