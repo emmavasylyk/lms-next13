a5d006e83db9273e956cba166108334740489c5e
emmavasylyk <emma.ovsiannikova91@gmail.com>
2024-05-23 14:08:51 +0300

    fix: sidebar-mobile

--- app/(dashboard)/_components/mobile-sidebar.tsx
+++ app/(dashboard)/_components/mobile-sidebar.tsx
@@ -1,15 +1,34 @@
+"use client";
+
 import { Menu } from "lucide-react";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Sidebar } from "./sidebar";
 
 export const MobileSidebar = () => {
+  const path = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [path]);
+
   return (
-    <Sheet>
-      <SheetTrigger className="md:hidden pr-4 hover:opacity-75 transition">
+    <Sheet open={isOpen}>
+      <SheetTrigger
+        onClick={() => setIsOpen((prev) => !prev)}
+        className="pr-4 transition hover:opacity-75 md:hidden"
+      >
         <Menu />
       </SheetTrigger>
-      <SheetContent side="left" className="p-0 bg-white">
+      <SheetContent
+        onEscapeKeyDown={() => setIsOpen(false)}
+        side="left"
+        onClose={() => setIsOpen(false)}
+        className="bg-white p-0"
+      >
         <Sidebar />
       </SheetContent>
     </Sheet>
